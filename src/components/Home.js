e80c638e6bb0f0e9f7cbf72eeac4f4f0df9a74f2
Davis <cyrilldavis22@gmail.com>
2021-10-13 18:19:37 +0530

    text changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
         <>
         <div className='container flex mx-auto w-4/5 py-24 items-center justify-between'>
             <div className='w-1/2'>
-                <h6 className='text-lg'><em>Are you hungry?</em></h6>
+                <h6 className='text-lg'><em>Are you starving?</em></h6>
                 <h1 className='font-bold text-6xl'>Don't wait!</h1>
                 <button className='bg-yellow-500 font-bold text-white my-4 px-4 py-2 rounded-full'>Order Now</button>
             </div>
